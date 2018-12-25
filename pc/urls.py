de8b5422c9059fb6a3d2149cdbb2dac3c3429451
ttshmily <ttshmilyfc@163.com>
2018-12-25 12:15:59 +0800

    example

--- pc/urls.py
+++ pc/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from rest_framework import routers
+from polls import views
+
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
+    url(r'^', include(router.urls)),
     url(r'^admin/', admin.site.urls),
     url(r'^polls/', include('polls.urls')),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from polls.serializers import UserSerializer, GroupSerializer
+from django.http import HttpResponse
 
 # Create your views here.
-from django.http import HttpResponse
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
