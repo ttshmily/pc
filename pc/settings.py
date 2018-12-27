d4703f07a8f65a55b5e927b678c141121574a234
ttshmily <ttshmilyfc@163.com>
2018-12-27 13:07:13 +0800

    move to mysql db

--- pc/settings.py
+++ pc/settings.py
@@ -76,9 +76,18 @@ WSGI_APPLICATION = 'pc.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # },
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bookinfo',
+        'USER': 'root',
+        'PASSWORD': '861031',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
