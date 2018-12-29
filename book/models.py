dde79d040eeba4255759210f754fc274f25c6b42
ttshmily <ttshmilyfc@163.com>
2018-12-29 14:20:06 +0800

    modify model

--- book/models.py
+++ book/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 # Create your models here.
 class BookInformation(models.Model):
-    btitle = models.CharField(max_length=20, verbose_name='名称')
-    bpub_date = models.DateField(verbose_name='发布日期', null=True)
-    bread = models.IntegerField(default=0, verbose_name='阅读量')
-    bcomment = models.IntegerField(default=0, verbose_name='评论量')
-    image = models.ImageField(upload_to='book_cover', verbose_name='图片', null=True)
+    title = models.CharField(max_length=20, verbose_name='名称')
+    pub_date = models.DateField(verbose_name='发布日期', null=True)
+    read_count = models.IntegerField(default=1, verbose_name='阅读量')
+    comment_count = models.IntegerField(default=0, verbose_name='评论量')
+    cover = models.ImageField(upload_to='book_cover', verbose_name='图片', null=True)
