286d78db891c9cc2bbeee39665dc4812ab561cce
ttshmily <ttshmilyfc@163.com>
2019-01-20 12:58:26 +0800

    sleep in entrypoint

--- start_django.sh
+++ start_django.sh
@@ -4,4 +4,4 @@ python3 manage.py migrate
 
 python3 manage.py collectstatic --noinput
 
-uwsgi --ini uwsgi.ini && tail -f uwsgiServer.log
+uwsgi --ini uwsgi.ini && sleep 5 && tail -f uwsgiServer.log
